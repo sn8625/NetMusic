1ddd6a10121d48dc856e9701ce875bd969a7ca4f
sn8625 <sn8625@gmail.com>
2019-11-26 23:55:30 +0800

    add Thread

--- NetMusicUI.py
+++ NetMusicUI.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # coding:utf-8
 
+from PyQt5.QtCore import QThread, pyqtSignal, QDateTime, QObject
+from PyQt5.QtWidgets import QApplication, QDialog, QLineEdit
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import QCoreApplication
 from PyQt5.Qt import *
 import download
 import sys
@@ -13,17 +13,29 @@ import GetMusic
 import time
 import threading
 
+class Work(QThread):
+    trigger = QtCore.pyqtSignal(str)
+
+    def run(self):
+        sec = 0
+        while 1:
+            self.trigger.emit('正在下载....已进行' + str(sec) + '秒')
+            time.sleep(1)
+            sec += 1
+
 class NetMusicUI(download.Ui_DownloadMusic):
     def __init__(self, DownloadMusic):
         super(download.Ui_DownloadMusic,self).__init__()
         super().setupUi(DownloadMusic)
         super().retranslateUi(DownloadMusic)
         self.cwd = os.getcwd()
+        self.timer = QTimer()
         self.loopNum = True
 
         self.btn_export_dir.clicked.connect(self.btnExportDir)
-        self.btn_start.clicked.connect(self.btnStart)
+        self.btn_start.clicked.connect(self.downMusicStart)
         self.btn_close.clicked.connect(QCoreApplication.instance().quit)
+        self.listurl = self.line_list.text()
 
     def btnExportDir(self):
         try:
@@ -35,40 +47,22 @@ class NetMusicUI(download.Ui_DownloadMusic):
             traceback.print_exc()
 
     def downMusicStart(self):
-        self.listurl = self.line_list.text()
-        self.loopNum = True
         try:
             down = GetMusic.GetMusicNet(self.listurl, self.savedir)
             down.DownMusic()
+            # runtime = False
         except Exception:
             traceback.print_exc()
-        self.loopNum = False
 
-    def LoadLog(self):
-        text_s = 0
-        while self.loopNum:
-            with open(self.savedir + '/log.txt') as f:
-                text = f.readlines()
-                text_l = len(text)
-                text_e = text_l
-                if text_e > text_s:
-                    text_r = text[text_s:text_e]
-                    for i in range(text_e):
-                        # self.text_export_log.setText(text_r[i])
-                        print(text_r[i])
-                    text_s += 1
-                f.close()
-                time.sleep(1)
+        self.backlog = Work()
+        self.backlog.trigger.connect(self.LoadLog)
+        self.thread = QThread()
+        self.backlog.moveToThread(self.thread)
+        self.thread.started.connect(self.backlog.run)
+        self.backlog.start()
 
-    def btnStart(self):
-        os.system('cls')
-        try:
-            # t1 = threading.Thread(target=self.downMusicStart)
-            t2 = threading.Thread(target=self.LoadLog)
-            # t1.start()
-            t2.start()
-        except Exception:
-            traceback.print_exc()
+    def LoadLog(self, sec):
+        self.text_export_log.setText(sec)
 
 app = QtWidgets.QApplication(sys.argv)
 MainUiWindow = QtWidgets.QMainWindow()
